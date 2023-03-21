a042bc8d964bfc031b456cca9eec5b5287f95168
Dblm0 <dmitrievdmitriymsk@gmail.com>
2023-03-21 17:31:48 +0300

    introduce RTIC to usb_uart_adapter

--- usb_uart_adapter/src/main.rs
+++ usb_uart_adapter/src/main.rs
@@ -1,201 +1,232 @@
 #![no_std]
 #![no_main]
 
-use defmt::info;
-use defmt_rtt as _;
-use embedded_hal::digital::v2::OutputPin;
-use panic_probe as _;
+mod uart_config;
 
-use rp_pico::{
-    entry,
-    hal::{
+#[rtic::app(device = rp_pico::hal::pac, peripherals = true)]
+mod app {
+    use defmt::info;
+    use defmt_rtt as _;
+    use panic_probe as _;
+
+    use crate::uart_config::ConfigDTO;
+    use embedded_hal::PwmPin;
+    use rp_pico::hal::{
         self, pac,
         uart::{UartConfig, UartPeripheral},
         usb::UsbBus,
         Clock,
-    },
-};
+    };
+    use usb_device::{
+        class_prelude::UsbBusAllocator,
+        prelude::{UsbDevice, UsbDeviceBuilder, UsbVidPid},
+    };
+    use usbd_serial::SerialPort;
 
-mod uart_config;
-use uart_config::ConfigDTO;
-type UartConfigWithRate = (ConfigDTO, fugit::HertzU32);
-use usb_device::{
-    class_prelude::UsbBusAllocator,
-    prelude::{UsbDevice, UsbDeviceBuilder, UsbVidPid},
-};
-use usbd_serial::SerialPort;
+    type UartConfigWithRate = (ConfigDTO, fugit::HertzU32);
+    type EnabledUart0 = UartPeripheral<
+        hal::uart::Enabled,
+        pac::UART0,
+        (
+            hal::gpio::Pin<hal::gpio::bank0::Gpio0, hal::gpio::Function<hal::gpio::Uart>>,
+            hal::gpio::Pin<hal::gpio::bank0::Gpio1, hal::gpio::Function<hal::gpio::Uart>>,
+        ),
+    >;
+    type PwmLed = hal::pwm::Channel<hal::pwm::Pwm4, hal::pwm::FreeRunning, hal::pwm::B>;
 
-static mut USB_BUS_ALLOC: Option<UsbBusAllocator<UsbBus>> = None;
-static mut USB_DEV: Option<UsbDevice<UsbBus>> = None;
-static mut USB_SERIAL: Option<SerialPort<UsbBus>> = None;
-static mut UART_CONFIG: Option<UartConfigWithRate> = None;
-static mut UART: Option<EnabledUart0> = None;
-static mut LED: Option<LedPin> = None;
-const PID_VID: UsbVidPid = UsbVidPid(0x5678, 0x1234);
-
-type LedPin = hal::gpio::Pin<hal::gpio::bank0::Gpio25, hal::gpio::Output<hal::gpio::PushPull>>;
-type EnabledUart0 = UartPeripheral<
-    hal::uart::Enabled,
-    pac::UART0,
-    (
-        hal::gpio::Pin<hal::gpio::bank0::Gpio0, hal::gpio::Function<hal::gpio::Uart>>,
-        hal::gpio::Pin<hal::gpio::bank0::Gpio1, hal::gpio::Function<hal::gpio::Uart>>,
-    ),
->;
-fn init_statics() {
-    let mut pac = pac::Peripherals::take().unwrap();
-    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
-    let clocks = hal::clocks::init_clocks_and_plls(
-        rp_pico::XOSC_CRYSTAL_FREQ,
-        pac.XOSC,
-        pac.CLOCKS,
-        pac.PLL_SYS,
-        pac.PLL_USB,
-        &mut pac.RESETS,
-        &mut watchdog,
-    )
-    .ok()
-    .unwrap();
-
-    let usb = hal::usb::UsbBus::new(
-        pac.USBCTRL_REGS,
-        pac.USBCTRL_DPRAM,
-        clocks.usb_clock,
-        true,
-        &mut pac.RESETS,
-    );
-    unsafe { USB_BUS_ALLOC = Some(UsbBusAllocator::new(usb)) };
-    let alloc_ref = unsafe { USB_BUS_ALLOC.as_ref().unwrap() };
-
-    //init this first to evade BorrowMutError
-    let serial = SerialPort::new(alloc_ref);
-    unsafe {
-        USB_SERIAL = Some(serial);
+    #[shared]
+    struct Shared {
+        uart: Option<EnabledUart0>,
+        usb_serial: SerialPort<'static, UsbBus>,
     }
 
-    let device = UsbDeviceBuilder::new(alloc_ref, PID_VID)
-        .manufacturer("Dblm0")
-        .product("USB UART ADAPTER")
-        .serial_number("123")
-        .device_class(2) // from: https://www.usb.org/defined-class-codes
-        .build();
-    unsafe {
-        USB_DEV = Some(device);
+    const TX_SIZE: usize = 32;
+    const RX_SIZE: usize = 64;
+    #[local]
+    struct Local {
+        led: PwmLed,
+        delay: cortex_m::delay::Delay,
+        uart_config: UartConfigWithRate,
+        usb_dev: UsbDevice<'static, UsbBus>,
     }
+    const PID_VID: UsbVidPid = UsbVidPid(0x5678, 0x1234);
 
-    let sio = hal::Sio::new(pac.SIO);
-
-    let pins = rp_pico::Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut pac.RESETS,
-    );
-    let uart_pins = (
-        pins.gpio0.into_mode::<hal::gpio::FunctionUart>(),
-        pins.gpio1.into_mode::<hal::gpio::FunctionUart>(),
-    );
-
-    unsafe {
-        let rate = clocks.peripheral_clock.freq();
-        UART_CONFIG = Some((ConfigDTO::default(), rate));
-    }
-
-    let conf = unsafe { UART_CONFIG.as_ref().unwrap() };
-    let rate = conf.1;
-    let conf = UartConfig::from(&conf.0);
-    let uart = UartPeripheral::new(pac.UART0, uart_pins, &mut pac.RESETS)
-        .enable(conf, rate)
+    #[init(local = [USB_ALLOCATOR: Option<UsbBusAllocator<UsbBus>> = None])]
+    fn init(c: init::Context) -> (Shared, Local, init::Monotonics) {
+        let mut pac = c.device;
+        let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+        let clocks = hal::clocks::init_clocks_and_plls(
+            rp_pico::XOSC_CRYSTAL_FREQ,
+            pac.XOSC,
+            pac.CLOCKS,
+            pac.PLL_SYS,
+            pac.PLL_USB,
+            &mut pac.RESETS,
+            &mut watchdog,
+        )
+        .ok()
         .unwrap();
-    unsafe { UART = Some(uart) };
 
-    unsafe {
-        LED = Some(
-            pins.led
-                .into_push_pull_output_in_state(hal::gpio::PinState::Low),
+        let usb_alloc = UsbBusAllocator::new(hal::usb::UsbBus::new(
+            pac.USBCTRL_REGS,
+            pac.USBCTRL_DPRAM,
+            clocks.usb_clock,
+            true,
+            &mut pac.RESETS,
+        ));
+        c.local.USB_ALLOCATOR.replace(usb_alloc);
+        let usb_alloc = c.local.USB_ALLOCATOR.as_ref().unwrap();
+
+        //init this first to evade BorrowMutError
+        let usb_serial = SerialPort::new(usb_alloc);
+
+        let usb_dev = UsbDeviceBuilder::new(usb_alloc, PID_VID)
+            .manufacturer("Dblm0")
+            .product("USB UART ADAPTER")
+            .device_class(2) // from: https://www.usb.org/defined-class-codes
+            .build();
+
+        info!("USB Initialized");
+        let sio = hal::Sio::new(pac.SIO);
+
+        let pins = rp_pico::Pins::new(
+            pac.IO_BANK0,
+            pac.PADS_BANK0,
+            sio.gpio_bank0,
+            &mut pac.RESETS,
         );
+        let uart_pins = (
+            pins.gpio0.into_mode::<hal::gpio::FunctionUart>(),
+            pins.gpio1.into_mode::<hal::gpio::FunctionUart>(),
+        );
+
+        let rate = clocks.peripheral_clock.freq();
+        let uart_config = (ConfigDTO::default(), rate);
+
+        let mut uart = UartPeripheral::new(pac.UART0, uart_pins, &mut pac.RESETS)
+            .enable(UartConfig::from(&uart_config.0), uart_config.1)
+            .unwrap();
+        uart.enable_rx_interrupt();
+
+        let uart = Some(uart);
+
+        let delay = cortex_m::delay::Delay::new(c.core.SYST, clocks.system_clock.freq().to_Hz());
+
+        let mut pwm = hal::pwm::Slices::new(pac.PWM, &mut pac.RESETS).pwm4;
+        pwm.set_top(25000);
+        pwm.channel_b.output_to(pins.led);
+        pwm.set_ph_correct();
+        pwm.enable();
+
+        (
+            Shared { usb_serial, uart },
+            Local {
+                led: pwm.channel_b,
+                delay,
+                usb_dev,
+                uart_config,
+            },
+            init::Monotonics(),
+        )
     }
-}
-
-fn try_reconfigure_uart(
-    uart: EnabledUart0,
-    new_config: ConfigDTO,
-) -> Result<EnabledUart0, hal::uart::Error> {
-    let uart_config = UartConfig::from(&new_config);
-    let old_conf = unsafe { UART_CONFIG.as_ref().unwrap() };
-    let uart = uart.disable();
-
-    let result = uart.enable(uart_config, old_conf.1);
-    if result.is_ok() {
-        unsafe { UART_CONFIG = Some((new_config, old_conf.1)) }
-    }
-    result
-}
-fn led_blinked_routine(led: &mut LedPin, routine: impl FnOnce()) {
-    let _ = led.set_high();
-    routine();
-    let _ = led.set_low();
-}
-#[entry]
-fn main() -> ! {
-    init_statics();
-
-    let (usb, usb_sp) = unsafe { (USB_DEV.as_mut().unwrap(), USB_SERIAL.as_mut().unwrap()) };
-    let mut new_config: Option<ConfigDTO> = None;
-    info!("USB Initialized");
-
-    let mut usb_send_buf = [0u8; 128];
-    let mut usb_receive_buf = [0u8; 128];
-
-    let led_diag = unsafe { LED.as_mut().unwrap() };
-
-    loop {
-        if let Some(conf) = new_config {
-            unsafe {
-                if let Ok(reconfigured) = try_reconfigure_uart(UART.take().unwrap(), conf) {
-                    UART.replace(reconfigured);
-                    info!("UART reconfigured :{}", conf);
-                }
-            };
-            let _ = new_config.take();
-        }
-        let uart = unsafe { UART.as_ref().unwrap() };
+    #[idle(local = [delay, led])]
+    fn idle(c: idle::Context) -> ! {
         loop {
-            //Read data from UART and send it to USB Serial
-            //TODO:Fix data loss due to the pauses caused by defmt 
-            if let Ok(readen) = uart.read_raw(&mut usb_send_buf) {
-                led_blinked_routine(led_diag, || {
-                    if let Ok(written) = usb_sp.write(&usb_send_buf[0..readen]) {
-                        info!("{} of {} byte written to USB", written, readen);
+            for i in (0..=25000).skip(100) {
+                c.local.delay.delay_us(32);
+                c.local.led.set_duty(i);
+            }
+
+            for i in (0..=25000).rev().skip(100) {
+                c.local.delay.delay_us(32);
+                c.local.led.set_duty(i);
+            }
+            c.local.delay.delay_us(64);
+        }
+    }
+
+    #[task(
+        binds = UART0_IRQ,
+        priority = 2,
+        shared = [usb_serial, uart],
+        local = [usb_send_buf: [u8; TX_SIZE] = [0; TX_SIZE]]
+    )]
+
+    fn uart_irq(c: uart_irq::Context) {
+        (c.shared.usb_serial, c.shared.uart).lock(|usb_serial, uart| {
+            // Read data from UART and send it to USB Serial
+            let uart = uart.as_ref().unwrap();
+            let usb_send_buf = c.local.usb_send_buf;
+            //TODO:Fix data loss due to the pauses caused by defmt
+            if let Ok(readen) = uart.read_raw(usb_send_buf) {
+                if let Ok(written) = usb_serial.write(&usb_send_buf[0..readen]) {
+                    assert_eq!(written, readen);
+                    info!("{} of {} byte written to USB", written, readen);
+                }
+            }
+        });
+    }
+
+    #[task(
+        binds = USBCTRL_IRQ,
+        priority = 2,
+        shared = [usb_serial, uart],
+        local = [
+            usb_dev,
+            uart_config,
+            usb_receive_buf: [u8; RX_SIZE] = [0; RX_SIZE]
+        ],
+    )]
+    fn usbctrl_irq(mut c: usbctrl_irq::Context) {
+        let poll_res = c
+            .shared
+            .usb_serial
+            .lock(|usb_serial| c.local.usb_dev.poll(&mut [usb_serial]));
+
+        if !poll_res {
+            return;
+        }
+
+        //Read data from USB Serial and send it to UART
+        (&mut c.shared.usb_serial, &mut c.shared.uart).lock(|usb_serial, uart| {
+            let uart = uart.as_ref().unwrap();
+            let usb_receive_buf = c.local.usb_receive_buf;
+            if let Ok(readen) = usb_serial.read(usb_receive_buf) {
+                let mut data_to_send = &usb_receive_buf[0..readen];
+                let mut written = 0;
+
+                while !data_to_send.is_empty() {
+                    if let Ok(rem) = uart.write_raw(data_to_send) {
+                        written += data_to_send.len() - rem.len();
+                        data_to_send = rem;
                     }
+                }
+
+                info!("{} of {} byte written to UART", written, readen);
+            }
+        });
+
+        let new_conf = c
+            .shared
+            .usb_serial
+            .lock(|usb_serial| ConfigDTO::try_from(usb_serial.line_coding()));
+
+        match new_conf {
+            Ok(new) if new != c.local.uart_config.0 => {
+                c.shared.uart.lock(|uart_container| {
+                    let mut uart = uart_container.take().unwrap();
+                    uart.disable_rx_interrupt();
+                    let uart = uart.disable();
+                    let mut uart = uart
+                        .enable(UartConfig::from(&new), c.local.uart_config.1)
+                        .unwrap();
+                    uart.enable_rx_interrupt();
+                    uart_container.replace(uart);
                 });
+                c.local.uart_config.0 = new;
+                info!("UART reconfigured :{}", c.local.uart_config.0);
             }
-
-            if usb.poll(&mut [usb_sp]) {
-                //Read data from USB Serial and send it to UART
-                if let Ok(readen) = usb_sp.read(&mut usb_receive_buf) {
-                    let mut data_to_send = &usb_receive_buf[0..readen];
-                    let mut written = 0;
-
-                    while !data_to_send.is_empty() {
-                        led_blinked_routine(led_diag, || {
-                            if let Ok(rem) = uart.write_raw(data_to_send) {
-                                written += data_to_send.len() - rem.len();
-                                data_to_send = rem;
-                            }
-                        });
-                    }
-
-                    info!("{} of {} byte written to UART", written, readen);
-                }
-            }
-
-            if let Ok(conf) = ConfigDTO::try_from(usb_sp.line_coding()) {
-                if unsafe { conf != UART_CONFIG.unwrap().0 } {
-                    new_config = Some(conf);
-                    break;
-                }
-            }
+            _ => (),
         }
     }
 }
