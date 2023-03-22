9f031facb34a013620e521fed49d702d717a5525
Dblm0 <dmitrievdmitriymsk@gmail.com>
2023-03-22 18:10:53 +0300

    split uart to reader and writer

--- usb_uart_adapter/src/main.rs
+++ usb_uart_adapter/src/main.rs
@@ -24,19 +24,20 @@ mod app {
     use usbd_serial::SerialPort;
 
     type UartConfigWithRate = (ConfigDTO, fugit::HertzU32);
-    type EnabledUart0 = UartPeripheral<
-        hal::uart::Enabled,
-        pac::UART0,
-        (
-            hal::gpio::Pin<hal::gpio::bank0::Gpio0, hal::gpio::Function<hal::gpio::Uart>>,
-            hal::gpio::Pin<hal::gpio::bank0::Gpio1, hal::gpio::Function<hal::gpio::Uart>>,
-        ),
-    >;
+    type UartPins = (
+        hal::gpio::Pin<hal::gpio::bank0::Gpio0, hal::gpio::Function<hal::gpio::Uart>>,
+        hal::gpio::Pin<hal::gpio::bank0::Gpio1, hal::gpio::Function<hal::gpio::Uart>>,
+    );
+
+    type EnabledUart = hal::uart::UartPeripheral<hal::uart::Enabled, pac::UART0, UartPins>;
+    type UartReader = rp_pico::hal::uart::Reader<pac::UART0, UartPins>;
+    type UartWriter = rp_pico::hal::uart::Writer<pac::UART0, UartPins>;
     type PwmLed = hal::pwm::Channel<hal::pwm::Pwm4, hal::pwm::FreeRunning, hal::pwm::B>;
 
     #[shared]
     struct Shared {
-        uart: Option<EnabledUart0>,
+        uart_reader: Option<UartReader>,
+        uart_writer: Option<UartWriter>,
         usb_serial: SerialPort<'static, UsbBus>,
     }
 
@@ -105,7 +106,7 @@ mod app {
             sio.gpio_bank0,
             &mut pac.RESETS,
         );
-        let uart_pins = (
+        let uart_pins: UartPins = (
             pins.gpio0.into_mode::<hal::gpio::FunctionUart>(),
             pins.gpio1.into_mode::<hal::gpio::FunctionUart>(),
         );
@@ -118,7 +119,8 @@ mod app {
             .unwrap();
         uart.enable_rx_interrupt();
 
-        let uart = Some(uart);
+        let (uart_reader, uart_writer) = uart.split();
+        let (uart_reader, uart_writer) = (Some(uart_reader), Some(uart_writer));
 
         let delay = cortex_m::delay::Delay::new(c.core.SYST, clocks.system_clock.freq().to_Hz());
 
@@ -130,7 +132,11 @@ mod app {
 
         unsafe { GLOBAL_TIMER.replace(rp_pico::hal::Timer::new(pac.TIMER, &mut pac.RESETS)) };
         (
-            Shared { usb_serial, uart },
+            Shared {
+                usb_serial,
+                uart_reader,
+                uart_writer,
+            },
             Local {
                 led: pwm.channel_b,
                 delay,
@@ -159,11 +165,11 @@ mod app {
     #[task(
         binds = UART0_IRQ,
         priority = 2,
-        shared = [usb_serial, uart],
+        shared = [usb_serial, uart_reader],
         local = [usb_send_buf: [u8; BUF_SIZE] = [0; BUF_SIZE]]
     )]
     fn uart_irq(c: uart_irq::Context) {
-        (c.shared.usb_serial, c.shared.uart).lock(|usb_serial, uart| {
+        (c.shared.usb_serial, c.shared.uart_reader).lock(|usb_serial, uart| {
             // Read data from UART and send it to USB Serial
             let uart = uart.as_ref().unwrap();
             let usb_send_buf = c.local.usb_send_buf;
@@ -180,7 +186,7 @@ mod app {
     #[task(
         binds = USBCTRL_IRQ,
         priority = 1, //priority is less than in uart_irq task to prevent data losses
-        shared = [usb_serial, uart],
+        shared = [usb_serial, uart_writer, uart_reader],
         local = [
             usb_dev,
             uart_config,
@@ -198,7 +204,7 @@ mod app {
         }
 
         //Read data from USB Serial and send it to UART
-        (&mut c.shared.usb_serial, &mut c.shared.uart).lock(|usb_serial, uart| {
+        (&mut c.shared.usb_serial, &mut c.shared.uart_writer).lock(|usb_serial, uart| {
             let uart = uart.as_ref().unwrap();
             let usb_receive_buf = c.local.usb_receive_buf;
             if let Ok(readen) = usb_serial.read(usb_receive_buf) {
@@ -223,15 +229,18 @@ mod app {
 
         match new_conf {
             Ok(new) if new != c.local.uart_config.0 => {
-                c.shared.uart.lock(|uart_container| {
-                    let mut uart = uart_container.take().unwrap();
+                (c.shared.uart_reader, c.shared.uart_writer).lock(|r_container, w_container| {
+                    let (r, w) = (r_container.take().unwrap(), w_container.take().unwrap());
+                    let mut uart = EnabledUart::join(r, w);
                     uart.disable_rx_interrupt();
                     let uart = uart.disable();
                     let mut uart = uart
                         .enable(UartConfig::from(&new), c.local.uart_config.1)
                         .unwrap();
                     uart.enable_rx_interrupt();
-                    uart_container.replace(uart);
+                    let (r, w) = uart.split();
+                    r_container.replace(r);
+                    w_container.replace(w);
                 });
                 c.local.uart_config.0 = new;
                 info!("UART reconfigured :{}", c.local.uart_config.0);
